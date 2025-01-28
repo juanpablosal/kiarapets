7a2dd4e1ae0e16fc58150de9234ef7008b053020
juanpablosal <123312218+juanpablosal@users.noreply.github.com>
2025-01-28 13:49:29 -0500

    Kiara pets (frontend)

---
+++ date.js
@@ -0,0 +1,16 @@
+
+  // Configurar la fecha mínima como el día de hoy
+  const dateInput = document.getElementById("cita");
+
+  // Función para obtener la fecha de hoy en formato YYYY-MM-DD
+  function setMinDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    const minDate = `${year}-${month}-${day}`;
+    dateInput.setAttribute("min", minDate);
+  }
+
+  // Ejecutamos la función al cargar la página
+  setMinDate();
